06c64a60e4400dfcbfdf5277686ae660328612f7
Алина Широкова <147584671+pipichaan@users.noreply.github.com>
2024-10-24 23:04:54 +0300

    Add files via upload

---
+++ skachFILE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (WebClient wc = new WebClient())
+        {
+            string file = "https://vscode.ru/filesForArticles/test.docx";
+            string papka = @"C:\Users\alina\OneDrive\Рабочий стол\2 курс\системка\сохранениефайла\";
+            string name = "Тестовый файл.docx";
+            try
+            {
+                wc.DownloadFile(file, papka + name);
+                Console.WriteLine("файл скачен в " + papka + name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ошибка " + ex.Message);
+            }
+        }
+    }
+}
